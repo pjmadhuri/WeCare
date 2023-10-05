8e440cb2efd16a31d2ab9dbd055d125c636cc535
pjmadhuri <81244053+pjmadhuri@users.noreply.github.com>
2023-10-06 00:26:46 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -32,13 +32,5 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
+   
 };
